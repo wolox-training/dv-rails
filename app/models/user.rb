6b55ca3591dff82b53edbcb55d6c6345043dca44
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-07 11:58:02 -0500

    add trackable in the migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :trackable
   include DeviseTokenAuth::Concerns::User
 end
--- db/migrate/20181102224936_devise_token_auth_create_users.rb
+++ db/migrate/20181102224936_devise_token_auth_create_users.rb
@@ -3,6 +3,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     add_column :users, :tokens, :json
     add_column :users, :provider, :string, :null => false, :default => 'email'
     add_column :users, :uid, :string, :null => false, :default => ''
+    add_column :users, :sign_in_count, :integer, :default => 0, :null => false
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
     add_index :users, [:uid, :provider], unique: true
   end
 end
