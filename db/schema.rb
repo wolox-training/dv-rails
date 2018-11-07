28f0874c148cf5371d8ef1260519b35d7b2b8e84
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-07 17:32:52 -0500

    create model book and insert book

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_224936) do
+ActiveRecord::Schema.define(version: 2018_11_07_213227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "genero", default: "", null: false
+    t.string "autor", default: "", null: false
+    t.string "image", default: "", null: false
+    t.string "titulo", default: "", null: false
+    t.string "editor", default: "", null: false
+    t.string "año", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
