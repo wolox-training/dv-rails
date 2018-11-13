a27f302c0a8db165eddb250b7002751e4f95b848
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-13 15:10:48 -0500

    no offenses from rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :trackable
   include DeviseTokenAuth::Concerns::User
-  has_many :rents
+  has_many :rents, dependent: :destry
 end
