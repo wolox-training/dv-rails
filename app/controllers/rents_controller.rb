c6ebe6eda58871ffbe3f76a91ecf49832c4cafc9
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-21 15:53:52 -0500

    adjustments in the branch.

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,17 +1,17 @@
 class RentsController < ApplicationController
   include Wor::Paginate
   def create
-    @rent = Rent.new(rent_params)
-    if @rent.save
-      render json: @rent
+    rent = Rent.new(rent_params)
+    if rent.save
+      render json: rent, status: :created
     else
-      render json: { error: @rent.errors.messages }, status: :unprocessable_entity
+      render json: { error: rent.errors.messages }, status: :unprocessable_entity
     end
   end
 
   def index
-    @rents = Rent.where(user_id: params[:user_id])
-    render_paginated @rents
+    rents = Rent.where(user_id: params[:user_id])
+    render_paginated rents
   end
 
   private
