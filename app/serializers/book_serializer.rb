474c1ad8b817507320a1cf2cba5b129d3fd86ea5
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-21 09:36:58 -0500

    rent routes

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :author, :title, :image
+  attributes :id, :author, :title, :genre, :publisher, :year, :image
   has_many :rents
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,23 +2,21 @@ require 'rails_helper'
 
 describe BooksController do
   describe 'GET #index' do
-    subject(:http_request) do
-      get :index
-    end
+    subject(:index_request) { get :index}
 
     let!(:books) { create_list(:book, 70) }
 
-    it 'responses with the books json' do
-      expect(JSON.parse(http_request.body)['total_count']).to eq(Book.all.count)
+    it 'returns with the books json' do
+      expect(JSON.parse(index_request.body)['total_count']).to eq(Book.all.count)
     end
 
     it 'responds with 200 status' do
-      expect(http_request).to have_http_status(:ok)
+      expect(index_request).to have_http_status(:ok)
     end
   end
 
   describe 'GET #show' do
-    subject(:http_request) do
+    subject(:show_request) do
       get :show, params: { id: id }
     end
 
@@ -28,13 +26,13 @@ describe BooksController do
       let(:id) { book.id }
 
       it 'response one book in json' do
-        expect(http_request.body).to eq BookSerializer.new(
+        expect(show_request.body).to eq BookSerializer.new(
           book, root: false
         ).to_json
       end
 
       it 'responds with 200 status' do
-        expect(http_request).to have_http_status(:ok)
+        expect(show_request).to have_http_status(:ok)
       end
     end
 
@@ -42,7 +40,7 @@ describe BooksController do
       let(:id) { -1 }
 
       it 'responds with 404 if the book do not founded ' do
-        expect(http_request).to have_http_status(:not_found)
+        expect(show_request).to have_http_status(:not_found)
       end
     end
   end
