50e328cdc5300a8add3f6863282e9872cb3a2b60
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-23 17:32:12 -0500

    Internationalize email

--- app/workers/rent_worker.rb
+++ app/workers/rent_worker.rb
@@ -2,6 +2,7 @@ class RentWorker
   include Sidekiq::Worker
   def perform(rent_id)
     rent = Rent.find(rent_id)
+    I18n.locale = rent.user.locale
     RentMailer.with(rent: rent).rent_notification.deliver_now
   end
 end
