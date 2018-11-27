edee3d6d8ac7bb92f52efa16f1e9229472a7fb06
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-27 15:53:38 -0500

    Active admin and create, update, delete, the models user, rent and books

---
+++ app/admin/users.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register User do
+  permit_params %i[email first_name email last_name created_at updated_at provider uid locale]
+  form do |f|
+    inputs 'Details' do
+      input :email
+      input :first_name 
+      input :last_name
+      input :created_at
+      input :updated_at
+      input :provider
+      input :uid
+      input :locale
+      li "Created at #{f.object.created_at}" unless f.object.new_record?
+    end
+    actions
+  end
+end
