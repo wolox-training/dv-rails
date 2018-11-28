0eb2c220024ecb52c3d20c7e87fadfdb2ff12805
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-28 10:43:51 -0500

    fix conflict

--- spec/factories/book_suggestions.rb
+++ spec/factories/book_suggestions.rb
@@ -2,11 +2,10 @@ FactoryBot.define do
   factory :book_suggestion do
     synopsis { Faker::Lorem.sentence }
     price { Faker::Number.number(10) }
-    author { Faker::Book.author  }
+    author { Faker::Book.author }
     title { Faker::Book.title }
     link { Faker::Internet.url }
     publisher { Faker::Book.publisher }
     year { Faker::Number.between(1900, 2018) }
   end
 end
- 
