859641bbcc0a572cce4ac72b74535e435ce8f6b8
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-28 10:32:19 -0500

    adjustments to faker in book_suggestion

--- spec/factories/book_suggestions.rb
+++ spec/factories/book_suggestions.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :book_suggestion do
-    synopsis { 'MyString' }
-    price { 1.5 }
-    author { 'MyString' }
-    title { 'MyString' }
-    link { 'MyString' }
-    publisher { 'MyString' }
-    year { 'MyString' }
-    user { nil }
+    synopsis { Faker::Lorem.sentence }
+    price { Faker::Number.number(10) }
+    author { Faker::Book.author  }
+    title { Faker::Book.title }
+    link { Faker::Internet.url }
+    publisher { Faker::Book.publisher }
+    year { Faker::Number.between(1900, 2018) }
   end
 end
+ 
