411eacde29b8f7cac930cea50540288f08eec6b0
Mauricio Vasquez <diegovasquez@MacBook-Pro-77.local>
2018-11-28 10:53:36 -0500

    save shange save change in model the book

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :trackable
   include DeviseTokenAuth::Concerns::User
   has_many :rents, dependent: :destroy
+  has_many :book_suggestion, dependent: :destroy
 end
